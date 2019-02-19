663b7dd1c975b36ec87133b0509ba48e5927fab1
dengyunTong <308229360@qq.com>
2019-02-19 09:31:45 +0800

    require

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+require.config({
+    baseUrl: '/js/',
+    paths: {
+        'swiper': './libs/swiper.min',
+        'bscroll': './libs/bscroll'
+    }
+})
---
+++ src/js/page/index.js
@@ -0,0 +1,5 @@
+require(['./js/config.js'], function() {
+    require(['swiper', 'bscroll'], function(swiper, bscroll) {
+        console.log(swiper, bscroll)
+    })
+})
